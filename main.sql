dc0f37e83a6c5342dd25c6a47b7f9e5d67c08681
paultimothy12 <37688619+paultimothy12@users.noreply.github.com>
2022-10-05 12:16:46 +0530

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE books;
+
+CREATE TABLE books(
+    book_id INT NOT NULL AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+);
